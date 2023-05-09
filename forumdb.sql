c9af6d706ee7b6e6b066bc3427d975b0ec429536
Romeo <romeoestoy0101@gmail.com>
2023-05-09 16:11:07 -0700

    Upload sql

--- forumdb.sql
+++ forumdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 03, 2023 at 11:30 PM
+-- Generation Time: May 10, 2023 at 01:09 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `profile_pic` varchar(255) NOT NULL,
+  `role` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`, `profile_pic`, `role`) VALUES
+(1, 'admin1', '$2y$10$gHA7pSNNckAOQQrvYLmecuDkY2IuDcpE67JvLdQ3zUCdvbWDYRQEO', '', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `comments`
 --
 
@@ -40,14 +61,9 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`id`, `user_id`, `comment_id`, `comment`, `date`) VALUES
-(1, 5, 4, 'gsdgdsgsdfdsfsdfsdfsd', '2023-05-02 00:00:00'),
-(2, 5, 4, 'asdasdaszxczxcz', '2023-05-02 17:39:03'),
-(3, 5, 4, 'sadcvxclcklghkfgnfbcvb', '2023-05-02 17:39:18'),
-(4, 5, 4, 'Jiii', '2023-05-02 17:40:37'),
-(5, 6, 4, 'Hello', '2023-05-02 17:53:04'),
 (6, 6, 8, 'Hii Omee here', '2023-05-02 18:04:59'),
 (7, 6, 3, '3 post, 1 comment', '2023-05-02 18:05:17'),
-(8, 6, 9, 'Hello Ome', '2023-05-02 18:06:09');
+(12, 16, 3, 'Holla', '2023-05-06 20:39:23');
 
 -- --------------------------------------------------------
 
@@ -67,15 +83,12 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `user_id`, `post`, `date`) VALUES
-(1, 1, 'This is my first post', '2023-02-06 16:30:16'),
 (2, 1, 'this is my second post', '2023-02-06 16:49:15'),
 (3, 1, 'this is my third post', '2023-02-06 16:50:19'),
-(4, 1, 'this is my fourth post', '2023-02-06 17:07:56'),
-(5, 1, 'post number 5', '2023-02-06 17:17:48'),
 (6, 4, 'hiii', '2023-04-24 02:19:43'),
 (7, 4, 'Qwerty', '2023-04-24 02:25:24'),
-(8, 6, 'Hello World', '2023-05-02 18:04:07'),
-(9, 6, 'Hii Romeo', '2023-05-02 18:04:21');
+(8, 6, 'Hi World', '2023-05-07 07:58:47'),
+(12, 1, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum est, voluptatibus eos consequuntur iste rem possimus, corporis dolor maiores placeat minima itaque hic nisi earum. Voluptas voluptatibus in atque sequi.', '2023-05-05 17:33:32');
 
 -- --------------------------------------------------------
 
@@ -89,22 +102,32 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `date` date DEFAULT NULL,
-  `image` varchar(1024) DEFAULT NULL
+  `image` varchar(1024) DEFAULT NULL,
+  `role` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `date`, `image`) VALUES
-(1, 'Eathorne', 'email@email.com', '$2y$10$RFbYu7mI0HO9wdw9DOmUzOnJ.WQ5BXKdCQ1zBwvcn2p0jk/vuOX0W', '2023-02-06', NULL),
-(4, 'Mary', 'mary@email.com', '$2y$10$FNSKNLPVLIxayzO7kWlGB.jsRHm12s5/u04hMjXDHpJRwBb4v8I2m', '2023-02-06', NULL),
-(5, 'beqenuke', 'pacyvop@mailinator.com', '$2y$10$8HRIG10WTQpUTHT.UXFLY.APxea0rRvaw3R1/ljd7GkeRwHi8DdbW', '2023-05-02', NULL),
-(6, 'jarykajun', 'sezipev@mailinator.com', '$2y$10$3A7l6yTCD4R6g6SC9G4V9OBAjlDyWcyK37pynQqsRmxxe0j8gQMJ2', '2023-05-02', NULL);
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `date`, `image`, `role`) VALUES
+(1, 'Eathorne', 'email@email.com', '$2y$10$RFbYu7mI0HO9wdw9DOmUzOnJ.WQ5BXKdCQ1zBwvcn2p0jk/vuOX0W', '2023-02-06', 'assets/images/image1.png', 1),
+(4, 'Mary', 'mary@email.com', '$2y$10$FNSKNLPVLIxayzO7kWlGB.jsRHm12s5/u04hMjXDHpJRwBb4v8I2m', '2023-02-06', 'assets/images/image2.png', 2),
+(5, 'beqenuke', 'pacyvop@mailinator.com', '$2y$10$8HRIG10WTQpUTHT.UXFLY.APxea0rRvaw3R1/ljd7GkeRwHi8DdbW', '2023-05-02', 'assets/images/image3.jpeg', 1),
+(6, 'jarykajun', 'sezipev@mailinator.com', '$2y$10$YvY87myJmTYcQBSyA5dDxOO5M74sEpKEiYVT3rnvQTz5xrbP8wcAS', '2023-05-02', 'assets/images/q.jpg', 2),
+(14, 'belafa', 'xutivofu@mailinator.com', '$2y$10$x/8eZG5tWx0IgdMHDCsb7uRjm54gPS4aEPYSsBvSP2G3vL.CMGc3e', '2023-05-06', 'assets/images/image5.jpeg', 2),
+(15, 'zawira', 'ruhatelej@mailinator.com', '$2y$10$nYeDc3bxULmytCPE58TYoOFkBy/jGv/630uFO2hdfVyO2EIEM2O/S', '2023-05-06', 'assets/images/default.png', 1),
+(16, 'rylityxew', 'gibenyxilu@mailinator.com', '$2y$10$vKFHXTfG34E.7nbWYod3oOJNKjP/y5WuzAOD.imoRxQ6LukmC0hf6', '2023-05-06', 'assets/images/default.png', 2);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `comments`
@@ -131,22 +154,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
