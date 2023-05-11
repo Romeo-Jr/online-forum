55f980110c7d43190f1039240cb907f9df510896
Romeo <romeoestoy0101@gmail.com>
2023-05-11 05:02:12 -0700

    Upload code

--- index.php
+++ index.php
@@ -76,6 +76,7 @@
 
 			<section class="js-posts">
 				<?php
+
 					$query = "select users.username, users.image, posts.id, posts.post, posts.date from posts inner join users on posts.user_id = users.id ORDER BY posts.date DESC";
 					$row = query($query);
 					
@@ -158,13 +159,6 @@
 			</section>
  
 			<div class="class_37" style="display: flex;justify-content: space-between;" >
-				<button class="class_54"  >
-					Prev page
-				</button>
-				<div>Page 1</div>
-				<button class="class_39"  >
-					Next page
-				</button>
 
 			</div>
  
@@ -173,131 +167,5 @@
 		<?php include('signup.inc.php') ?>
 		<?php include('login.inc.php') ?>
 	</section>
-	<!--post card template-->
-	<div class="js-post-card hide class_42" style="animation: appear 3s ease;" >
-		<div class="class_45" >
-			<img src="assets/images/user.jpg" class="js-image class_47" >
-			<h2 class="js-username class_48" style="font-size:16px" >
-				Jane Name
-			</h2>
-		</div>
-		<div class="class_49" >
-			<h4 class="js-date class_41"  >
-				3rd Jan 23 14:35 pm
-			</h4>
-			<div class="js-post class_15"  >
-				is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets c
-			</div>
-			<div class="class_51" >
-				<i class="bi bi-chat-left-dots class_52">
-				</i>
-				<div class="js-comment-link class_53" style="color:blue;cursor: pointer;"  >
-					Comments
-				</div>
-			</div>
-		</div>
-	</div>
-	<!--end post card template-->
-	
 </body>
-
-<!-- <script>
-	
-	var mypost = {
-
-		submit: function(e){
-
-			e.preventDefault();
-			let text = document.querySelector(".js-post-input").value.trim();
-			if(text == ""){
-				alert("Please type something to post");
-				return;
-			}
-
-			let form = new FormData();
- 
-			form.append('post', text);
-			form.append('data_type', 'add_post');
-			var ajax = new XMLHttpRequest();
-
-			ajax.addEventListener('readystatechange',function(){
-
-				if(ajax.readyState == 4)
-				{
-					if(ajax.status == 200){
-
-						console.log(ajax.responseText);
-						let obj = JSON.parse(ajax.responseText);
-						alert(obj.message);
-
-						if(obj.success){
-							mypost.load_posts();
-						}
-					}else{
-						alert("Please check your internet connection");
-					}
-				}
-			});
-
-			ajax.open('post','ajax.inc.php', true);
-			ajax.send(form);
-		},
-
-		load_posts: function(e){
-
-			let form = new FormData();
- 
-			form.append('data_type', 'load_posts');
-			var ajax = new XMLHttpRequest();
-
-			ajax.addEventListener('readystatechange',function(){
-
-				if(ajax.readyState == 4)
-				{
-					if(ajax.status == 200){
-
-						console.log(ajax.responseText);
-						let obj = JSON.parse(ajax.responseText);
-
-						if(obj.success){
-							let post_holder = document.querySelector(".js-posts");
-
-							post_holder.innerHTML = "";
-							let template = document.querySelector(".js-post-card");
-							for (var i = 0; i < obj.rows.length; i++) {
- 
- 								template.querySelector(".js-post").innerHTML = obj.rows[i].post;
-								template.querySelector(".js-date").innerHTML = obj.rows[i].date;
-								template.querySelector(".js-comment-link").setAttribute('onclick',`mypost.view_comments(${obj.rows[i].id})`);
-								template.querySelector(".js-username").innerHTML = (typeof obj.rows[i].user == 'object') ? obj.rows[i].user.username : 'User';
-								
-								if(typeof obj.rows[i].user == 'object')
-									template.querySelector(".js-image").src = obj.rows[i].user.image;
-
-								let clone = template.cloneNode(true);
-								clone.classList.remove('hide');
-								
-								post_holder.appendChild(clone);
-							}
-
-						}
-					}
-				}
-			});
-
-			ajax.open('post','ajax.inc.php', true);
-			ajax.send(form);
-		},
-
-		view_comments: function(id){
-
-			window.location.href = "post.php?id="+id;
-		},
-
-
-	};
- 
-	mypost.load_posts();
-</script> -->
-
 </html>
